08c6dadb494b0812557fdd5c17872fc23b3aa464
Lynden Lim <lyndenlim15@gmail.com>
2022-02-04 23:52:27 -0500

    commit tree 0744c7d085fd9811a6737228ff8e433f9666e78f

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDeletedItem }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,37 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+    .then(resp => resp.json())
+    .then(() => handleDeletedItem(id))
+  }
+
+  function handleChange(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        "correctIndex" : e.target.value
+      })
+    })
+    .then(resp => resp.json())
+    .then(data => data)
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(resp => resp.json())
+      .then(questionData => setData(questionData))
+  }, [])
+
+  function handleDeletedItem(deletedItem) {
+    const leftovers = data.filter(item => {
+      if (item.id !== deletedItem) {
+        return item
+      }
+    })
+    setData(leftovers)
+  }
+
+  const dataElements = data.map(item => {
+    return <QuestionItem key={item.id} question={item} handleDeletedItem={handleDeletedItem}/>
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{dataElements}</ul>
     </section>
   );
 }
